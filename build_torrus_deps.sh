288baa5b71890af39755a9bcee724f076505ce8e
Tobias Oetiker <tobi@oetiker.ch>
2011-09-07 23:54:06 +0200

    * add note about compiling perl too * use standard FCGI * put thirdparty into PATH

--- build_torrus_deps.sh
+++ build_torrus_deps.sh
@@ -2,6 +2,10 @@
 
 . `dirname $0`/sdbs.inc
 
+echo "you may ./build_perl-5.14.1.sh to get a recent version of perl up"
+
+export PATH=$PREFIX/bin:$PATH
+
 simplebuild ftp://xmlsoft.org/libxml2/ libxml2-2.7.8.tar.gz --without-python
 
 if prepare http://download.oracle.com/berkeley-db/ db-4.8.30.tar.gz ; then
@@ -32,7 +36,7 @@ for module in \
         JSON::XS \
         JSON \
         CGI::Fast \
-        MSTROUT/FCGI-0.69.tar.gz \
+        FCGI \
 ; do
     perlmodule $module
 done
