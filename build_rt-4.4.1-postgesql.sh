37935db68ce59dfd32745283d8358b483670acef
Tobias Oetiker <tobi@oetiker.ch>
2016-08-05 00:40:10 +0200

    rt builder

--- build_rt-4.4.1-postgesql.sh
+++ build_rt-4.4.1-postgesql.sh
@@ -3,15 +3,13 @@
 export PERL=/usr/bin/perl
 
 . `dirname $0`/sdbs.inc
-
-simplebuild https://github.com/libgd/libgd/releases/download/gd-2.2.3/ libgd-2.2.3.tar.gz
-
 if prepare https://download.bestpractical.com/pub/rt/release/ rt-4.4.1.tar.gz; then
-    ./configure  --with-db-type=SQLite --enable-gd --enable-graphviz --prefix=$PREFIX --with-my-user-group 
+    ./configure  --with-db-type=Pg --enable-gd --enable-graphviz --prefix=$PREFIX \
+        --with-web-user=rt4 \
+        --with-web-group=rt4
     mkdir -p $PREFIX/bin
     wget --no-check-certificate -O $PREFIX/bin/cpanm cpanmin.us && chmod 755 $PREFIX/bin/cpanm
-
-    for module in `make testdeps | grep MISSING | awk '{print $1}'`; do
+    for module in `make testdeps | grep MISSING | awk '{print $1}' | grep -v SOME`; do
        perlmodule $module
     done
     perlmodule Starman
