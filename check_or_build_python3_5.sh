929cc8595bfff863173ad2f676907a3f8bc41d83
Stanislav Sinyagin <ssinyagin@k-open.com>
2020-08-03 07:48:22 +0000

    use prefix/lib64 for libraries on 64-bit systems

--- check_or_build_python3_5.sh
+++ check_or_build_python3_5.sh
@@ -18,13 +18,18 @@ build_python=yes
 
 if [ $build_python = yes ]; then
     if prepare https://www.openssl.org/source openssl-1.1.1g.tar.gz; then
-        CFLAGS="-O3 -fPIC" ./config --prefix=$PREFIX --openssldir=$PREFIX/ssl
+        CFLAGS="-O3 -fPIC" ./config --prefix=$PREFIX --openssldir=$PREFIX/ssl --libdir=${SDBS_LIBDIR}
         if CFLAGS="-O3 -fPIC" make install; then
             touch $WORKDIR/openssl-1.1.1g.tar.gz.ok
         fi
     fi
     
-    simplebuild https://www.python.org/ftp/python/3.8.5 Python-3.8.5.tgz LDFLAGS="-L$PREFIX/lib64 -L$PREFIX/lib"
+    simplebuild https://www.python.org/ftp/python/3.8.5 Python-3.8.5.tgz LDFLAGS="${R}${SDBS_LIBDIR} -L${SDBS_LIBDIR}"
+
+    # dirty hack because python cannot find its libraries in lib64
+    if [ ${SDBS_LIBDIR} = ${PREFIX}/lib64 ]; then
+        ln -s ${PREFIX}/lib64/python3.8/lib-dynload ${PREFIX}/lib/python3.8/lib-dynload
+    fi
 fi
 
 
